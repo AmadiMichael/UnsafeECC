dbed5e74356ec1bf16cefd905187db12c05edaeb
Michael Amadi <amadimichaeld@gmail.com>
2024-01-21 20:36:57 +0100

    add Parent Private Recovery in HD Wallets

---
+++ HDWalletParentPrivateKeyRecovery.py
@@ -0,0 +1,114 @@
+import hashlib
+import hmac
+from py_ecc.typing import PlainPoint2D
+from hexbytes import (
+    HexBytes,
+)
+from ECC import (
+    bytes_to_int,
+    privtopub,
+    add,
+    N, 
+    G,
+)
+
+
+
+# HELPER FUNCTIONS, EXPLOIT FUNCTION CAN BE FOUND BELOW
+
+def int_to_bytes(integer) -> int:
+    return integer.to_bytes((integer.bit_length() + 7) // 8, 'big')
+
+def compress_pub_key(pub_key: PlainPoint2D) -> bytes:
+        pub_key_parity_prefix = (2).to_bytes(1, 'big') if pub_key[1] % 2 == 0 else (3).to_bytes(1, 'big')
+        return pub_key_parity_prefix + pub_key[0].to_bytes(32, 'big')
+
+
+
+def derive(hd_parent_node_info: (PlainPoint2D, int, int), index: int, isHardened: bool) -> (PlainPoint2D, int, int):
+    parent_public_key = hd_parent_node_info[0]
+    parent_priv_key = hd_parent_node_info[1]
+    parent_chain_code = hd_parent_node_info[2]
+    
+    if isHardened:
+        index = 2147483648 + index
+        hash = hmac.new(parent_chain_code.to_bytes(32, 'big'), (0).to_bytes(1, 'big') + parent_priv_key.to_bytes(32, 'big') + index.to_bytes(4, 'big'), digestmod=hashlib.sha512).digest()
+    else:
+        hash = hmac.new(parent_chain_code.to_bytes(32, 'big'), compress_pub_key(parent_public_key) + index.to_bytes(4, 'big'), digestmod=hashlib.sha512).digest()
+    
+    offset = hash[0:32]
+    child_chaincode = bytes_to_int(hash[32:64])
+    pub_offset = privtopub(offset)
+
+    child_private_key = bytes_to_int(offset) + parent_priv_key
+    child_pub_key = add(parent_public_key, pub_offset)
+    
+    return (child_pub_key, child_private_key, child_chaincode)
+
+
+
+# This works for non hardened nodes because the hash is determined from the parent chaincode, parent public key and child index, if we have all three and the child private key we can easily get the parent private key
+# Does not work hardened nodes because the hash if determined from the parent chaincode, parent private key and child index, and trying to get the parent private key by using the parent private key defeats the purpose
+# 
+# THE OBVIOUS QUESTION IS WHY NOT HARDEN EVERYTHING
+# One benefit of HD wallets is being able to generate child addresses by only having the parent public key and chain code. This is helpful for audits and generating one time addresses on an untrusted server
+#
+# How paths are defined "m / purpose' / coin_type' / account' / change / address_index" (an apostrophe means it's a hardened path)
+# All child wallets from the account derivation path and above are hardened, the change path and address_index path is not to facilitate the above use case
+
+
+# INDEPTH
+#
+# The exploit works as follows
+# Since
+#   child_private_key = offset + parent_private_key
+# that means that
+#   parent_private_key = child_private_key - offset
+# 
+# This means that if we can get the `offset` value and any `child_private_key` derived from this parent node, we can get the `parent_private_key` and hence all possible child private keys, 
+# specifically referring to the private key of the leaked child wallet's siblings.
+#
+#
+# First let's see how we can get `offset`
+# Recall that for non-hardened child wallets
+#   offset = sha512({k: chainCode, v: compressedPublicKey.append(index as bytes4))
+# 
+# Notice how these parameters are not explicitly secret values or advertised to always be private
+# This means that if we can get the parent's `chainCode`, the parent's public key and the index of the `child_private_key` we have the we can easily get the `offset`
+#
+# For the `child_private_key`, that's not going to be as easy to get as any of the above but let's assume we have 1 for the POC below
+
+
+# values from base derivation path "m" using Bitcoin seed and mnemonic "critic august page curtain lion scene poverty over empty system lady useless"
+global_hd_master_priv = 7689162646347254789289457585570777995548838195840484586546417750201678994478
+global_hd_master_pub = privtopub(int_to_bytes(global_hd_master_priv))
+global_chaincode = 104326670430597193981057684893345542428774993972637944294968933122272829988816
+global_hd_master_node_info = (global_hd_master_pub, global_hd_master_priv, global_chaincode)
+
+# derive paths
+# m/44'
+m_44prime = derive(global_hd_master_node_info, 44, True)
+# m/44'/60'
+m_44prime_60prime = derive(m_44prime, 60, True)
+# m/44'/60'/0'
+m_44prime_60prime_0prime = derive(m_44prime_60prime, 0, True)
+# m/44'/60'/0'/0
+m_44prime_60prime_0prime_0 = derive(m_44prime_60prime_0prime, 0, False)
+# m/44'/60'/0'/0/0
+m_44prime_60prime_0prime_0_0 = derive(m_44prime_60prime_0prime_0, 0, False)
+
+
+
+# Exploit function
+def get_parent_private_key(parent_pub_key: PlainPoint2D, child_private_key: int, index_of_child: int, parent_chain_code: int) -> int:
+    hash = hmac.new(parent_chain_code.to_bytes(32, 'big'), compress_pub_key(parent_pub_key) + index_of_child.to_bytes(4, 'big'), digestmod=hashlib.sha512).digest()
+    
+    offset = bytes_to_int(hash[0:32])
+    parent_priv_key = (child_private_key - offset) % N
+    
+    return parent_priv_key
+
+
+# Exploit
+recoveredParentPublicKey = get_parent_private_key(m_44prime_60prime_0prime_0[0], m_44prime_60prime_0prime_0_0[1], 0, m_44prime_60prime_0prime_0[2])
+assert(recoveredParentPublicKey == m_44prime_60prime_0prime_0[1])
--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Unsafe ECC
 
-Proof of concept and explainers to ECC vulnerabilities. It doesn't just tell you "Don't do this, it is exploitable. Do this rather', it also shows you how exactly it's exploitable and how the formula is derived.
+This collection comprises proof of concept demonstrations and explanatory content on vulnerabilities associated with Elliptic Curve Cryptography (ECC). Instead of merely advising against certain practices with a generic warning, it goes further to illustrate precisely how vulnerabilities can be exploited and outlines the derivation of the underlying formulas.
 
-## Includes
+## Contents
 
-- Nonce reuse: Covers how, given two signatures `s1` and `s2` of messages `m1` and `m2` respectively, signed by a private key `p` whose public key is `q`, a malicious actor can calculate and get `p` using just `s1`, `s2`, `m1` and `m2`
-- Nonce Leak: Covers how, given a signature `s` of message `m`, signed using nonce `k` by a private key `p` whose public key is `q`, a malicious actor can calculate and get `p` using just `s`, `m`, and `k`
-- Fake signatures : Covers how, given a private key `p` and a public key `q`, a malicious actor can generate a signature and message pair that recovers to `q` without the exploiter knowing or having access to `p`
-- Signature malleability: Covers how given a signature (`v`, `r` and `s`) of message `m` signed by private key `p` whose public key is `q`, a malicious actor can subtract `s` from the order of the curve and flip `v` to 27 if it's 28 or 28 if it's 27 to get a new signature that recovers to `q` (no need to have access to `p`)
+- Nonce Reuse: Explores the scenario where, given two signatures (`s1` and `s2`) of messages (`m1` and `m2`) respectively, signed by a private key (`p`) with the corresponding public key (`q`), a malicious actor can calculate and obtain `p` using only `s1`, `s2`, `m1`, and `m2`.
+- Nonce Leak: Examines the situation where, with a signature `s` of message `m` signed using nonce `k` by a private key (`p`) with the corresponding public key (`q`), a malicious actor can calculate and obtain `p` using just `s`, `m`, and `k`.
+- Fake Signatures: Explores how, with knowledge of a private key (`p`) and its corresponding public key (`q`), a malicious actor can generate a signature and message pair that resolves to `q` without the exploiter needing to know or have access to `p`.
+- Signature Malleability: Covers the process wherein, given a signature (`v`, `r`, and `s`) of a message (`m`) signed by a private key (`p`) with the corresponding public key (`q`), a malicious actor can manipulate the signature by subtracting `s` from the curve's order and flipping `v` to 27 if it's 28, or 28 if it's 27, resulting in a new signature that resolves to `q` without requiring access to `p`.
+- Parent Private Recovery in HD Wallets: Addresses the scenario where, given knowledge of a parent public key and its chain code, if there is a leak of any non-hardened child wallet's private key along with the index of that child wallet, it's trivial to calculate the parent's private key. Which in turn enables derivation of all possible child private keys, specifically referring to the private key of the leaked child wallet's siblings.
